4eb935355e29bc96c80618321a651010b3fa6dac
wangzizhao <81192@stu.sjzu.edu.cn>
2021-01-01 23:20:47 +0800

    Fixing some bugs!
    Add two file organizing functions, each with its own advantages and disadvantages!

--- Processing-of-genomic-data/main.cpp
+++ Processing-of-genomic-data/main.cpp
@@ -11,18 +11,19 @@
 using namespace std;
 
 void dataRead();
-void DataC();
+void dataRead2();
 
 int main(int argc, char** argv)
 {
-    dataRead();
+    dataRead2();
     return 0;
 }
+//Read and write function one
 void dataRead()
 {
     try
     {
-        char buffer[256000];
+        string buffer;
         fstream  afile("D:\\smart_match\\Processing-of-genomic-data\\Processing-of-genomic-data\\Align1.txt");
         fstream  afile1("D:\\smart_match\\Processing-of-genomic-data\\Processing-of-genomic-data\\Align2.txt");
         if(! afile.is_open())
@@ -30,50 +31,57 @@ void dataRead()
             cout << "Error opening file";
             exit(1);
         }
+        int i=0;
         while (!afile.eof())
-            afile>>buffer;
-        for(int i=0;i<strlen(buffer);i++)
         {
-           // afile1<<buffer[i];
-           // if((i+1)%1000==0)
-           //    afile1<<"\n";
+            afile>>buffer;
+            for(i=0;i<buffer.length();i+=1000)
+            {
+                afile1<<buffer.substr(i,1000);
+                if(i+1000<=buffer.length())
+                    afile1<<endl;
+            }
         }
         afile.close();
-    }
-    catch (exception except)
-    {
-        cout<<"文件打开失败！"<<"\n";
-    }
-}
-void DataC()
-{
-    try
-    {
-        char szTest[1000] = {0};
-        int len = 0;
-
-        FILE *fp = fopen("D:\\smart_match\\Processing-of-genomic-data\\Processing-of-genomic-data\\Align1.txt", "r");
-        if(NULL == fp)
-        {
-            printf("failed to open dos.txt\n");
-            exit(1) ;
-        }
-
-        while(!feof(fp))
-        {
-            memset(szTest, 0, sizeof(szTest));
-            fgets(szTest, sizeof(szTest) - 1, fp); // 包含了换行符
-            printf("%s", szTest);
-        }
-
-        fclose(fp);
-
+        afile1.close();
     }
     catch (exception ex)
     {
-
+        cout<<"File open failed!"<<"The Reason is:"<<endl<<ex.what();
     }
-
-
 }
+//Read and write function two
+void dataRead2()
+{
+    try
+    {
+        char buffer;
+        fstream  afile("D:\\smart_match\\Processing-of-genomic-data\\Processing-of-genomic-data\\Align1.txt");
+        fstream  afile1("D:\\smart_match\\Processing-of-genomic-data\\Processing-of-genomic-data\\Align2.txt");
+        if(!afile.is_open())
+        {
+            cout << "Error opening file";
+            exit(1);
+        }
+        int i=0;
+        while (!afile.eof())
+        {
+            afile>>buffer;
+            if(buffer=='A'||buffer=='G'||buffer=='C'||buffer=='T')
+            {
+                i++;
+                afile1<<buffer;
+                if(i%1000==0)
+                    afile1<<endl;
+            }
+        }
+        afile.close();
+        afile1.close();
+    }
+    catch (exception ex)
+    {
+        cout<<"File open failed!"<<"The Reason is:"<<endl<<ex.what();
+    }
+}
+
 
